1f12cfc451bd09c362e8c94aed84156bb0dfeb6a
arafimam -Git <syedarafimam27@gmail.com>
2022-06-27 15:19:37 -0700

    FInal Files

---
+++ index.js
@@ -0,0 +1,39 @@
+var typed = new Typed('.anim',{
+    strings:[
+        'Software Engineering',
+        'Full stack development',
+        'Competitive Programming',
+        'Robotics',
+        'Hardware'
+    ],
+    typeSpeed:50,
+    backSpeed:50,
+    loop:true
+});
+
+var typed = new Typed('.anim1',{
+    strings:[
+        'Started on April 25, 2022',       
+    ],
+    typeSpeed:50,
+    backSpeed:50,
+    loop:true
+});
+
+var typed = new Typed('.anim2',{
+    strings:[
+        'July 2021 to April 2022',       
+    ],
+    typeSpeed:50,
+    backSpeed:50,
+    loop:true
+});
+
+var typed = new Typed('.anim3',{
+    strings:[
+        'Jan 2021 to Oct 2021',       
+    ],
+    typeSpeed:50,
+    backSpeed:50,
+    loop:true
+});
